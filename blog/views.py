8d4a86edb800b9896f2f956247dbd83af031ddc3
perejoan <perejoan2005223@gmail.com>
2026-02-23 12:32:32 +0100

    Commit Test

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# blog/views.py
+
+from django.shortcuts import render
+
+
+def home(request):
+    """Home page view."""
+    # request = the incoming HTTP request (URL, method, headers, user info, etc.)
+    # render() loads the template file, returns it as an HTTP response
+    return render(request, 'blog/home.html')
