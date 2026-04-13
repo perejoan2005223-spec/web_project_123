6e6368b0fd006d832f4e2ecdfd24d13f8b2320fc
perejoan <perejoan2005223@gmail.com>
2026-04-13 11:43:08 +0200

    Sign up page added

--- blog/urls.py
+++ blog/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     path('', views.home, name='home'),  # '' = root URL (/), name='home' lets us reference it in templates
     path('posts/', views.PostListView.as_view(), name='post_list'),        # .as_view() turns the class into a callable view
     path('posts/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),  # <int:pk> captures the post ID from the URL
+
+    path('signup/', views.PostSignUpView.as_view(), name='signup'), # Sign up page
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 # blog/views.py
-
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
-from django.views.generic import DetailView, ListView  # generic views that handle common patterns
+from django.views.generic import DetailView, ListView, CreateView  # generic views that handle common patterns
 
 from .models import Post
 
@@ -19,4 +19,10 @@ class PostListView(ListView):
 class PostDetailView(DetailView):
     model = Post                            # which table to query
     template_name = 'blog/post_detail.html'
-    context_object_name = 'post'            # variable name in the template (default would be 'object')
+    context_object_name = 'post'    # variable name in the template (default would be 'object')
+
+
+class PostSignUpView(CreateView):
+    form_class = UserCreationForm
+    success_url = '/login/'
+    template_name = 'blog/signup.html'
