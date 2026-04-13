5a3ca4c281612bdd14d6cdc694b0cfc96004ddfe
perejoan <perejoan2005223@gmail.com>
2026-04-13 19:08:51 +0200

    Web Online

--- blog/admin.py
+++ blog/admin.py
@@ -3,7 +3,7 @@
 from django.contrib import admin
 from .models import Department, Professor, Title, Subject, Prof_Subject, Publication, Professor_Publication, Review
 
-# Registramos los nuevos modelos para poder verlos y editarlos en el panel de Admin de Django.
+# Registramos los nuevos modelos para poder verlos y editarlos en el panel de Admin de Django..
 admin.site.register(Department)
 admin.site.register(Professor)
 admin.site.register(Title)
