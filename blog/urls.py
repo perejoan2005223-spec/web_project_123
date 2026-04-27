0b9ec9154ba26429cb93860d19fb0e77fccd2236
perejoan <perejoan2005223@gmail.com>
2026-04-27 11:45:28 +0200

    Crear/Elminar/Editar Instàncies

--- blog/urls.py
+++ blog/urls.py
@@ -1,17 +1,16 @@
-# blog/urls.py
-
 from django.urls import path
-
 from . import views
 
-app_name = 'blog'  # namespace: lets us write {% url 'blog:home' %} in templates
+app_name = 'blog'
 
 urlpatterns = [
-    path('', views.home, name='home'),  # '' = root URL (/), name='home' lets us reference it in templates
-
-    path('signup/', views.PostSignUpView.as_view(), name='signup'), # Sign up page
-
+    path('', views.home, name='home'),
+    path('signup/', views.PostSignUpView.as_view(), name='signup'),
     path('profesores/', views.ProfessorListView.as_view(), name='professor_list'),
-
     path('profesores/<int:pk>/', views.ProfessorDetailView.as_view(), name='professor_detail'),
+
+    # Noves URLs per al CRUD de Review
+    path('review/nova/', views.ReviewCreateView.as_view(), name='review_create'),
+    path('review/<int:pk>/edita/', views.ReviewUpdateView.as_view(), name='review_update'),
+    path('review/<int:pk>/elimina/', views.ReviewDeleteView.as_view(), name='review_delete'),
 ]
