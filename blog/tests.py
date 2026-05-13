061f0644075cb88e6d6192d111cfafe41af66024
ikfas <iker.farre@iestremp.cat>
2026-05-13 19:16:30 +0200

    Tests

--- blog/tests.py
+++ blog/tests.py
@@ -7,11 +7,9 @@ from .models import Department, Professor, Subject, Prof_Subject, Review
 class ReviewCRUDTests(TestCase):
 
     def setUp(self):
-        # 1. Creem els usuaris de prova necessaris per a l'autenticació
         self.user = User.objects.create_user(username='alumne1', password='password123')
         self.other_user = User.objects.create_user(username='alumne2', password='password123')
 
-        # 2. Creem les dades de l'entorn segons els models del projecte
         self.department = Department.objects.create(name_dept="Enginyeria Informàtica")
         self.professor = Professor.objects.create(
             department=self.department,
@@ -36,7 +34,7 @@ class ReviewCRUDTests(TestCase):
             'prof_subject': self.prof_subject.id,
             'overall_rating': 8,
             'difficulty_rating': 6,
-            'comment': 'Molt bon professor, s’explica molt bé.'
+            'comment': "Molt bon professor, s'explica molt bé."
         }
 
         response = self.client.post(reverse('blog:review_create'), data)
@@ -44,7 +42,7 @@ class ReviewCRUDTests(TestCase):
         self.assertEqual(Review.objects.count(), 1)
 
         review = Review.objects.first()
-        self.assertEqual(review.comment, 'Molt bon professor, s’explica molt bé.')
+        self.assertEqual(review.comment, "Molt bon professor, s'explica molt bé.")
         self.assertEqual(review.user, self.user)
 
     def test_crear_review_anonim(self):
@@ -68,30 +66,3 @@ class ReviewCRUDTests(TestCase):
             difficulty_rating=5,
             comment="Comentari original"
         )
-
-        self.client.login(username='alumne1', password='password123')
-
-        updated_data = {
-            'prof_subject': self.prof_subject.id,
-            'overall_rating': 9,
-            'difficulty_rating': 5,
-            'comment': "Comentari modificat"
-        }
-
-        # S'utilitza la ruta de creació o edició segons correspongui
-        response = self.client.post(reverse('blog:review_create'), updated_data)
-        review.refresh_from_db()
-
-    def test_eliminar_propia_review(self):
-        """Test per esborrar una ressenya existent."""
-        review = Review.objects.create(
-            user=self.user,
-            prof_subject=self.prof_subject,
-            overall_rating=6,
-            difficulty_rating=6,
-            comment="Això s'eliminarà"
-        )
-
-        self.client.login(username='alumne1', password='password123')
-        Review.objects.filter(pk=review.pk).delete()
-        self.assertEqual(Review.objects.count(), 0)
